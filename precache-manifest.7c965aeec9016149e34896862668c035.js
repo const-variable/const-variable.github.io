937304adb99dbdfbacc9b1ce17826ff9eaf02061
const-variable <maulikchaudhary5@gmail.com>
2019-09-07 21:21:31 +0530

    Updates

---
+++ precache-manifest.7c965aeec9016149e34896862668c035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee150217fb40ff8c21bc7f353e07a735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ec0cb007bf1bf1f87e9",
+    "url": "/static/css/main.b633cf03.chunk.css"
+  },
+  {
+    "revision": "8aa340ed6e0332eed670",
+    "url": "/static/js/2.6481d611.chunk.js"
+  },
+  {
+    "revision": "0ec0cb007bf1bf1f87e9",
+    "url": "/static/js/main.570b794e.chunk.js"
+  },
+  {
+    "revision": "89b36c744b53f3aee7fc",
+    "url": "/static/js/runtime~main.9a4ec378.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
